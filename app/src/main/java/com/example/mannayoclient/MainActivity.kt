72f3662386c9414d61e1b0ae3f2facce29678dd3
hjk <hjkwon0814@naver.com>
2022-04-20 02:14:58 +0900

    retrofit api

--- app/src/main/java/com/example/mannayoclient/MainActivity.kt
+++ app/src/main/java/com/example/mannayoclient/MainActivity.kt
@@ -5,6 +5,14 @@ import android.os.Bundle
 import android.text.InputType
 import androidx.appcompat.app.AppCompatActivity
 import com.example.mannayoclient.databinding.ActivityMainBinding
+import com.google.gson.Gson
+import com.google.gson.internal.GsonBuildConfig
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.*
 
 
 class MainActivity : AppCompatActivity() {
@@ -15,6 +23,34 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
 
         setContentView(binding.root)
+
+        val retrofit = Retrofit.Builder()
+            .baseUrl("http://192.168.0.2:8080")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+
+
+        binding.button2.setOnClickListener {
+            val requestdata = reqdata(
+                realname = binding.editTextTextPersonName.text.toString(),
+                nickname = binding.editphone.text.toString()
+            )
+
+            val service = retrofit.create(mannayoService::class.java)
+            service.getMyAccount(requestdata).enqueue(object :Callback<resdata>
+            {
+                override fun onFailure(call: Call<resdata>, t: Throwable) {
+                    println("failed")
+                }
+
+                override fun onResponse(call: Call<resdata>, response: Response<resdata>) {
+                    println("***" + requestdata.realname + requestdata.nickname + "***")
+                    println("success")
+                }
+            })
+        }
+
 
         binding.button6.setOnClickListener {
             val intent = Intent(this, PasswordActivity::class.java)
@@ -35,3 +71,15 @@ class MainActivity : AppCompatActivity() {
         }
     }
 }
+
+public interface  mannayoService {
+    @POST("/members/findMyAccountByNickname")
+    fun getMyAccount(@Body reqdata: reqdata): Call<resdata>
+}
+data class reqdata (
+    val nickname: String,
+    val realname: String
+        )
+data class resdata(
+    val email : String
+)
