e6a850ca87ace95ec82b84aa475bdafecc0fda34
minji <mzmzk.11.20@gmail.com>
2022-04-23 23:20:00 +0900

    login page

--- app/src/main/java/com/example/mannayoclient/MainActivity.kt
+++ app/src/main/java/com/example/mannayoclient/MainActivity.kt
@@ -1,15 +1,17 @@
 package com.example.mannayoclient
 
+import android.content.Context
 import android.os.Bundle
 import android.text.InputType
+import android.text.TextUtils.replace
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.commit
 import androidx.navigation.fragment.findNavController
-import com.example.mannayoclient.databinding.ActivityMainBinding
-import com.example.mannayoclient.databinding.IdFragBinding
-import com.example.mannayoclient.databinding.PwFragBinding
+import com.example.mannayoclient.databinding.*
 import com.google.gson.annotations.Expose
 import com.google.gson.annotations.SerializedName
 import retrofit2.Call
@@ -17,6 +19,14 @@ import retrofit2.http.*
 
 class IdFragment : Fragment(R.layout.id_frag){
     lateinit var binding2: IdFragBinding
+    lateinit var mainActivity: MainActivity
+
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        mainActivity = context as MainActivity
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
@@ -39,9 +49,12 @@ class IdFragment : Fragment(R.layout.id_frag){
         }
 
 
+
         binding2.pwbutton.setOnClickListener {
-                findNavController().navigate(R.id.action_idFragment_to_pwFragment)
-         }
+            mainActivity.onFragmentChange(1)
+        }
+
+
         binding2.submit.setOnClickListener {
             findNavController().navigate(R.id.action_idFragment_to_idFragment2)
         }
@@ -53,6 +66,13 @@ class IdFragment : Fragment(R.layout.id_frag){
 
 
 class PwFragment : Fragment(R.layout.pw_frag) {
+    lateinit var mainActivity: MainActivity
+
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        mainActivity = context as MainActivity
+    }
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
@@ -71,19 +91,95 @@ class PwFragment : Fragment(R.layout.pw_frag) {
         }
 
         binding3.idbutton.setOnClickListener {
-            findNavController().navigate(R.id.action_pwFragment_to_idFragment)
+            mainActivity.onFragmentChange(0)
+        }
+
+        binding3.submit.setOnClickListener {
+            findNavController().navigate(R.id.action_pwFragment_to_PWFragment2)
+        }
+
+
+    }
+
+}
+
+class IdFragment2 : Fragment(R.layout.id2_frag) {
+    lateinit var binding6: Id2FragBinding
+    lateinit var mainActivity: MainActivity
+
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        mainActivity = context as MainActivity
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
+        super.onViewCreated(view, savedInstanceState)
+        binding6 = Id2FragBinding.bind(view)
+
+        binding6.pwbutton.setOnClickListener {
+            mainActivity.onFragmentChange(1)
+        }
+
+    }
+}
+
+class PWFragment2 : Fragment(R.layout.pw2_frag) {
+    lateinit var binding4: Pw2FragBinding
+    lateinit var mainActivity: MainActivity
+
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        mainActivity = context as MainActivity
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
+        super.onViewCreated(view, savedInstanceState)
+        binding4 = Pw2FragBinding.bind(view)
+
+        binding4.idbutton.setOnClickListener {
+            mainActivity.onFragmentChange(0)
+        }
+
+
+    }
+
+}
+
+class LoginFragment : Fragment(R.layout.login_frag)  {
+    lateinit var binding5: LoginFragBinding
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding5 = LoginFragBinding.bind(view)
+
+        binding5.textView2.setOnClickListener {
+            findNavController().navigate(R.id.action_loginFragment_to_idFragment)
         }
 
     }
 
 }
-class IdFragment2 : Fragment(R.layout.id2_frag)
-
-
 
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
+
+    fun onFragmentChange(index:Int) {
+        when(index){
+            0->supportFragmentManager.commit{
+                setReorderingAllowed(true)
+                replace(R.id.fragment, IdFragment::class.java, null)
+
+            }
+            1->supportFragmentManager.commit{
+                setReorderingAllowed(true)
+                replace(R.id.fragment, PwFragment::class.java, null)
+            }
+        }
+
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -91,7 +187,6 @@ class MainActivity : AppCompatActivity() {
 
 
         setContentView(binding.root)
-
 
 
 
