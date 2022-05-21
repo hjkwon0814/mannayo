6bfa5e4642bdddac827ef9ec9c5dd3ac5c82650f
hjk <hjkwon0814@naver.com>
2022-05-21 23:07:12 +0900

    Image Post Created

--- app/src/main/java/com/example/mannayoclient/ProfileFragment.kt
+++ app/src/main/java/com/example/mannayoclient/ProfileFragment.kt
@@ -7,20 +7,27 @@ import android.content.Context
 import android.content.Intent
 import android.content.SharedPreferences
 import android.content.pm.PackageManager
+import android.content.res.Resources
 import android.database.Cursor
 import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.ImageDecoder
 import android.net.Uri
 import android.os.Build
 import android.os.Bundle
 import android.provider.MediaStore
 import android.text.Editable
 import android.text.TextWatcher
+import android.util.Base64.NO_WRAP
+import android.util.Base64.encodeToString
 import android.view.LayoutInflater
 import android.view.View
 import android.widget.Button
 import android.widget.ImageView
+import android.widget.Toast
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import androidx.core.net.toFile
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.example.mannayoclient.databinding.ProfileFragBinding
@@ -32,18 +39,23 @@ import okhttp3.RequestBody
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+import java.io.ByteArrayOutputStream
 import java.io.File
 import java.io.FileOutputStream
+import java.io.InputStream
 import java.lang.Exception
+import java.net.URI
 import java.text.SimpleDateFormat
+import java.util.*
 
 
 class ProfileFragment : Fragment(R.layout.profile_frag) {
     lateinit var binding: ProfileFragBinding
     lateinit var mainActivity: SecondActivity
     lateinit var sharedPreferences: SharedPreferences
-    lateinit var path: String
-
+    var path: Uri? = null
+    var realPath : String? = null
+    var file: File? = null
     val CAMERA_PERMISSION = arrayOf(android.Manifest.permission.CAMERA)
     val STORAGE_PERMISSION = arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE,
         android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
@@ -64,8 +76,8 @@ class ProfileFragment : Fragment(R.layout.profile_frag) {
         mainActivity = context as SecondActivity
 
 
-
         sharedPreferences = mainActivity.getSharedPreferences("Pref", Context.MODE_PRIVATE)
+        val editor = sharedPreferences.edit()
         val id = sharedPreferences.getString("id", "0")?.toLongOrNull()
 
 
@@ -75,44 +87,27 @@ class ProfileFragment : Fragment(R.layout.profile_frag) {
         }
         //완료누르면 메인홈으로
         binding.completion.setOnClickListener {
-
-            path = binding.photo.getTag(0).toString()
             //creating a file
-            val file = File(path)
-            var fileName = id.toString()
-            fileName += ".png"
-
-            var requestBody : RequestBody = RequestBody.create(MediaType.parse("image/*"),file)
-            var body : MultipartBody.Part = MultipartBody.Part.createFormData("uploaded_file",fileName,requestBody)
-
-            retrofitService.service.setNickname(id ,binding.editTextTextPersonName2.text.toString()).enqueue(object : Callback<ReceiveOK> {
-                override fun onResponse(call: Call<ReceiveOK>, response: Response<ReceiveOK>) {
-                    if(response.isSuccessful) {
-                        println("닉네임등록 성공")
-                        retrofitService.service.setMyProfileImage(id, body).enqueue(object : Callback<ReceiveOK> {
-                            override fun onResponse(
-                                call: Call<ReceiveOK>,
-                                response: Response<ReceiveOK>
-                            ) {
-                                val receive = response.body() as ReceiveOK
-                                if(response.isSuccessful) {
-                                    println("프로필 등록 성공")
-                                }
-                            }
-
-                            override fun onFailure(call: Call<ReceiveOK>, t: Throwable) {
-                                println("프로필등록 실패")
-                            }
-
-                        })
-                    }
+            if(realPath != null) {
+                file = File(realPath)
+                if(!binding.editTextTextPersonName2.text.isNullOrEmpty()) {
+                    sendNickNameAndImage(id, realPath)
+                    editor.putString("nickname", binding.editTextTextPersonName2.text.toString())
+                    editor.commit()
+                    mainActivity.FragmentViewFromProfileToMainHome()
+                }else {
+                    Toast.makeText(activity, "닉네임을 입력해주세요", Toast.LENGTH_SHORT).show()
                 }
-
-                override fun onFailure(call: Call<ReceiveOK>, t: Throwable) {
-                    println("닉네임등록 실패")
+            } else {
+                if(!binding.editTextTextPersonName2.text.isNullOrEmpty()) {
+                    sendNickNameOnly(id, binding.editTextTextPersonName2.text.toString())
+                    editor.putString("nickname", binding.editTextTextPersonName2.text.toString())
+                    editor.commit()
+                    mainActivity.FragmentViewFromProfileToMainHome()
+                }else {
+                    Toast.makeText(activity, "닉네임을 입력해주세요", Toast.LENGTH_SHORT).show()
                 }
-
-            })
+            }
 
         }
 
@@ -148,11 +143,13 @@ class ProfileFragment : Fragment(R.layout.profile_frag) {
 
         val alertDialog = mBuilder.show()
         alertDialog.findViewById<View>(R.id.camera)?.setOnClickListener{
-            if(isPermitted(CAMERA_PERMISSION)){
+            if(isPermitted(CAMERA_PERMISSION) && isPermitted(STORAGE_PERMISSION)){
                 openCamera()
             }else {
                 ActivityCompat.requestPermissions(requireActivity(), CAMERA_PERMISSION, FLAG_PERM_CAMERA)
-                ActivityCompat.requestPermissions(requireActivity(), STORAGE_PERMISSION, FLAG_PERM_STORAGE)
+                if(isPermitted(CAMERA_PERMISSION)) {
+                    openCamera()
+                }
             }
             alertDialog.dismiss()
         }
@@ -219,25 +216,24 @@ class ProfileFragment : Fragment(R.layout.profile_frag) {
         if (resultCode == Activity.RESULT_OK) {
             when (requestCode) {
                 FLAG_REQ_CAMERA -> {
-                    if (data?.extras?.get("data") != null) {
-                        val bitmap = data?.extras?.get("data") as Bitmap
-                        val filename = newFileName()
-                        val uri = saveImageFile(filename, "image/jpg", bitmap)
-                        binding.photo.setImageBitmap(bitmap)
+                    val myBitmap = data?.extras?.get("data") as Bitmap
+                    if(myBitmap != null) {
+                        val uri = data?.data
+                        val imagePath = getImageUri(mainActivity, myBitmap)
+                        path = imagePath
+                        binding.photo.setImageBitmap(myBitmap)
                     }
                 }
                 FLAG_REQ_GALLERY -> {
-                    val uri =data?.data
-                    try {
-                        val contentResolver = requireActivity().contentResolver
-                        val bitmap = MediaStore.Images.Media.getBitmap(contentResolver, uri)
-                        binding.photo.setImageBitmap(bitmap)
-                    }catch (e:Exception) {
-                        e.printStackTrace()
-                    }
-
+                    val uri = data?.data
+                    // 이미지 뷰에 선택한 이미지 출력
+                    val imageview: ImageView = binding.photo
+                    imageview.setImageURI(uri)
+                    realPath = getRealPathFromURI(uri!!)
                 }
             }
+        }else if(resultCode == Activity.RESULT_CANCELED) {
+            Toast.makeText(mainActivity,"사진 선택 취소",Toast.LENGTH_LONG).show()
         }
     }
 
@@ -274,6 +270,80 @@ class ProfileFragment : Fragment(R.layout.profile_frag) {
                 }
             }
         }
+    }
+
+    fun getImageUri(inContext : Context?, inImage: Bitmap?): Uri? {
+        val btyes = ByteArrayOutputStream()
+        if(inImage != null) {
+            inImage.compress(Bitmap.CompressFormat.JPEG, 100, btyes)
+        }
+        val path = MediaStore.Images.Media.insertImage(inContext?.contentResolver, inImage, "Title" + " - " + Calendar.getInstance().time,null)
+        return Uri.parse(path)
+    }
+
+    fun getRealPathFromURI(contentURI : Uri) : String? {
+        val result : String?
+        val cursor : Cursor? = requireActivity().contentResolver.query(contentURI,null,null,null,null)
+
+        if(cursor == null) {
+            result = contentURI?.path
+        } else {
+            cursor.moveToFirst()
+            val idx = cursor.getColumnIndex(MediaStore.Images.ImageColumns.DATA)
+            result = cursor.getString(idx)
+            cursor.close()
+        }
+
+        return result
+    }
+
+    fun sendNickNameAndImage(id : Long? ,path : String?) {
+        var requestBody : RequestBody = RequestBody.create(MediaType.parse("multipart/form-data"),file)
+        var body : MultipartBody.Part = MultipartBody.Part.createFormData("multipartFile",file?.name,requestBody)
+
+        retrofitService.service.setNickname(id ,binding.editTextTextPersonName2.text.toString()).enqueue(object : Callback<ReceiveOK> {
+            override fun onResponse(call: Call<ReceiveOK>, response: Response<ReceiveOK>) {
+                if(response.isSuccessful) {
+                    println("닉네임등록 성공")
+                    retrofitService.service.setMyProfileImage(id, body).enqueue(object : Callback<ReceiveOK> {
+                        override fun onResponse(
+                            call: Call<ReceiveOK>,
+                            response: Response<ReceiveOK>
+                        ) {
+                            val receive = response.body() as ReceiveOK
+                            if(response.isSuccessful) {
+                                mainActivity.FragmentViewFromProfileToMainHome()
+                            }
+                        }
+
+                        override fun onFailure(call: Call<ReceiveOK>, t: Throwable) {
+                            println("프로필등록 실패")
+                        }
+
+                    })
+                }
+            }
+
+            override fun onFailure(call: Call<ReceiveOK>, t: Throwable) {
+                println("닉네임등록 실패")
+            }
+
+        })
+    }
+
+    fun sendNickNameOnly(id : Long?, nickname : String) {
+        retrofitService.service.setNickname(id ,binding.editTextTextPersonName2.text.toString()).enqueue(object : Callback<ReceiveOK> {
+            override fun onResponse(call: Call<ReceiveOK>, response: Response<ReceiveOK>) {
+                if(response.isSuccessful) {
+                    println("닉네임등록 성공")
+                }
+            }
+
+            override fun onFailure(call: Call<ReceiveOK>, t: Throwable) {
+                println("닉네임등록 실패")
+            }
+
+        })
     }
 
 
